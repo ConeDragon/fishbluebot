95c31d91ac6a807f1a5dcad0774572f95ffecc20
CuboidRaptor <fanjas112358@gmail.com>
2022-02-15 22:32:32 -0500

    yay
    added Mango- I mean MongoDB connection to bot (sorry I need to stop making that joke) also changed gitignore

--- bot.py
+++ bot.py
@@ -1,14 +1,40 @@
 #Imports
+#Requirements:
+#pip install py-cord dotenv pymongo
 import discord
 import os
 import sys
+import random
+import certifi
 
 from discord.ext import commands, tasks
 from discord.utils import get
 from dotenv import load_dotenv
+from pymongo import *
+
+#Import all speedy jsons, use default json module as failsafe
+try:
+    import ujson as json
+    
+except (ModuleNotFoundError, ImportError):
+    try:
+        import simplejson as json
+        
+    except (ModuleNotFoundError, ImportError):
+        import json
 
 #Load Environment variables
 load_dotenv()
+
+#Mango- I mean, MongoDB stuff
+client = MongoClient(
+    str(
+        os.getenv(
+            "MANGO"
+        )
+    ),
+    tlsCAFile=certifi.where()
+)
 
 #Bot stuff
 pf = "fb."
@@ -18,6 +44,13 @@ bot = commands.Bot(
     strip_after_prefix=True,
     intents=intents
 )
+
+#Reading from file
+with open("dat.json", "r") as f:
+    #Load crap from data file
+    yeetus = json.loads(f.read())
+    m8answers = yeetus["8ball"]
+    del yeetus
 
 # --Functions--
 
@@ -49,6 +82,21 @@ async def on_ready():
 async def ping(ctx):
     """Ping"""
     await ctx.send("pong")
+
+@bot.command(aliases=["8ball"])
+async def magic8ball(ctx, *args):
+    global m8answers
+    await ctx.send(
+        m8answers[
+            ord(
+                os.urandom(
+                    1
+                )
+            ) % len(
+                m8answers
+            )
+        ]
+    )
     
 @bot.command()
 async def killswitch(ctx):
@@ -61,4 +109,10 @@ async def killswitch(ctx):
     else:
         await ctx.send("rude why are you trying to kill me >:(")
 
-bot.run(str(os.getenv("DISCORD_TOKEN")))
+bot.run(
+    str(
+        os.getenv(
+            "DISCORD_TOKEN"
+        )
+    )
+)
